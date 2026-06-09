96874759813925c3325a0e7a9a393940bcdcedac
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-06-09 03:18:44 +0000

    Added products and featured products list rendering

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,19 @@
-import ProductCard from "./ProductCard"
+import ProductCard from "./ProductCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+
+    const [proudcts, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await axios.get("featured.json");
+            setProducts(response.data);
+        }
+        fetchProducts();
+    }, [])
+
     return <>
         <header className="bg-primary text-white text-center py-5">
             <div className="container">
@@ -12,37 +25,22 @@ export default function HomePage() {
         <main className="container my-5">
             <h2 className="text-center mb-4">Featured Products</h2>
             <div className="row">
-                <div className="col-md-3 mb-4">
-                    <ProductCard
-                        productName="Product 1"
-                        price={22.99}
-                        imageUrl="https://picsum.photos/id/8/200/300"
+                {
+                    proudcts.map(p => (
+                        <div className="col-md-3 mb-4" key={p.id}>
+                            <ProductCard
+                                productName={p.name}
+                                price={p.price}
+                                imageUrl={p.imageUrl}
 
-                    />
-                </div>
-                <div className="col-md-3 mb-4">
-                    <ProductCard
-                        productName="Product 2"
-                        price={33.99}
-                        imageUrl="https://picsum.photos/id/144/200/300"
-                    />
-                </div>
-                <div className="col-md-3 mb-4">
-                    <ProductCard
-                        productName="Product 3"
-                        price={133.99}
-                        imageUrl="https://picsum.photos/id/11/200/300"
+                            />
+                        </div>
+                    ))
+                }
 
-                    />
-                </div>
-                <div className="col-md-3 mb-4">
-                    <ProductCard
-                        productName="Product 4"
-                        price={533.99}
-                        imageUrl="https://picsum.photos/id/12/200/300"
 
-                    />
-                </div>
+
+
             </div>
         </main>
     </>
--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import ProductCard from "./ProductCard";
+
 export default function ProductPage() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        const fetchProducts = async () => {
+            const response = await axios.get("products.json");
+            setProducts(response.data);
+        }
+        fetchProducts();
+    },[])
+
     return <>
-        <h1>Products</h1>
+        <div className="container my-5">
+            <h1 className="text-center mb-4">Our Products</h1>
+            <div className="row">
+                {
+                    products.map( p => (
+                        <div key={p.id} className="col-md-4 mb-4">
+                            <ProductCard
+                                name={p.name}
+                                price={p.price}
+                                imageUrl={p.imageUrl}
+                            />
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     </>
 }
